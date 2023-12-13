7ec9dd93b06a4ea21ba811dc611690a107da3900
ragnvald <ragnvald@mindland.com>
2023-12-13 18:09:29 +0100

    Two new buttons!

--- code/user_interface.py
+++ code/user_interface.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 import subprocess
-import webbrowser  # Import the webbrowser module
+import webbrowser
 from PIL import Image, ImageTk
 
 # Function to open a web link
@@ -10,49 +10,50 @@ def open_link(url):
 
 # Function to load and display the image
 def display_image(bottom_frame):
-    # Load the image from the specified path
-    image_path = 'data_resources/mesa_illustration.png'  # Update this path if necessary
+    image_path = 'system_resources/mesa_illustration.png'
     image = Image.open(image_path)
-    
-    # Resize the image to 200x200 pixels using LANCZOS filter (previously ANTIALIAS)
     image = image.resize((200, 200), Image.Resampling.LANCZOS)
-    
-    # Convert the image to a PhotoImage object
     photo = ImageTk.PhotoImage(image)
-    
-    # Create a Label widget to display the image and center it in the bottom frame
     label = tk.Label(bottom_frame, image=photo)
-    label.image = photo  # Keep a reference to the image object
-    label.pack(side='bottom', pady=10)  # Center the image
+    label.image = photo
+    label.pack(side='bottom', pady=10)
 
 def import_assets():
     try:
         subprocess.run(["python", "01_import_asset_objects.py"], check=True)
-        messagebox.showinfo("Import assets", "Import assets script executed.")
     except subprocess.CalledProcessError:
         messagebox.showerror("Error", "Failed to execute import assets script.")
 
 def import_grids():
     try:
         subprocess.run(["python", "01_import_geocode_objects.py"], check=True)
-        messagebox.showinfo("Import grids (geocode objects)", "Import grids script executed.")
     except subprocess.CalledProcessError:
         messagebox.showerror("Error", "Failed to execute import grids script.")
+
+def edit_asset_group():
+    try:
+        subprocess.run(["python", "04_edit_asset_group.py"], check=True)
+    except subprocess.CalledProcessError:
+        messagebox.showerror("Error", "Failed to execute edit asset group script.")
+
+def edit_geocode_group():
+    try:
+        subprocess.run(["python", "04_edit_geocode_group.py"], check=True)
+    except subprocess.CalledProcessError:
+        messagebox.showerror("Error", "Failed to execute edit geocode group script.")
 
 def edit_susceptibilitiesandimportance():
     try:
         subprocess.run(["python", "04_edit_input.py"], check=True)
-        messagebox.showinfo("Edit asset data", "Edit susceptibilities and importance.")
     except subprocess.CalledProcessError:
         messagebox.showerror("Error", "Failed to edit.")
-    
+
 def view_statistics():
     messagebox.showinfo("View Statistics", "View statistics script executed.")
 
 def process_data():
     try:
         subprocess.run(["python", "06_tbl_mesa_stacked.py"], check=True)
-        messagebox.showinfo("Process data", "Spatial data processing")
     except subprocess.CalledProcessError:
         messagebox.showerror("Error", "Failed to process.")
 
@@ -65,9 +66,7 @@ def exit_program():
 # Setup the main Tkinter window
 root = tk.Tk()
 root.title("MESA 4")
-
-# Set the initial size of the window (width x height)
-root.geometry("540x540")  # Adjust the width and height as needed
+root.geometry("700x540")
 
 # Create a top frame for text and links
 top_frame = tk.Frame(root)
@@ -76,11 +75,9 @@ top_frame.pack(fill='x', expand=False, pady=10)
 # Add text and link to the top frame
 info_text = tk.Label(top_frame, text="Read more about the MESA method and tools", font=("Calibri", 10))
 info_text.pack(side='left')
-
 link_text = tk.Label(top_frame, text="here ", font=("Calibri", 10, "underline"), fg="blue", cursor="hand2")
 link_text.pack(side='left')
 link_text.bind("<Button-1>", lambda e: open_link("https://www.mesamethod.org/wiki/Main_Page"))
-
 
 # Main frame
 main_frame = tk.Frame(root)
@@ -88,14 +85,13 @@ main_frame.pack(fill='both', expand=True, pady=20)
 main_frame.grid_columnconfigure(0, weight=1)
 main_frame.grid_columnconfigure(1, weight=1)
 
-
 # Left panel
 left_panel = tk.Frame(main_frame)
 left_panel.grid(row=0, column=0, sticky="ew", padx=20)
 left_panel.grid_rowconfigure(0, weight=1)
 left_panel.grid_rowconfigure(1, weight=1)
 left_panel.grid_rowconfigure(2, weight=1)
-left_panel.grid_rowconfigure(3, weight=1)  # Extra row for spacing
+left_panel.grid_rowconfigure(3, weight=1)
 
 # Right panel
 right_panel = tk.Frame(main_frame)
@@ -103,22 +99,25 @@ right_panel.grid(row=0, column=1, sticky="ew", padx=20)
 right_panel.grid_rowconfigure(0, weight=1)
 right_panel.grid_rowconfigure(1, weight=1)
 right_panel.grid_rowconfigure(2, weight=1)
-right_panel.grid_rowconfigure(3, weight=1)  # Extra row for spacing to make it visually apealing
+right_panel.grid_rowconfigure(3, weight=1)
 
 bottom_frame = tk.Frame(root)
 bottom_frame.pack(fill='x', expand=False)
 
-# Button width
-button_width = 20  # Adjust as needed
-button_padx = 20  # Padding around buttons on x-axis
-button_pady = 10  # Padding around buttons on y-axis
+button_width = 20
+button_padx = 20
+button_pady = 10
 
 # Add buttons to left panel with spacing between buttons
 import_assets_btn = tk.Button(left_panel, text="Import Assets", command=import_assets, width=button_width)
 import_assets_btn.grid(row=0, column=0, padx=button_padx, pady=button_pady)
+edit_asset_group_btn = tk.Button(left_panel, text="Edit Asset Group", command=edit_asset_group, width=button_width)
+edit_asset_group_btn.grid(row=0, column=1, padx=button_padx, pady=button_pady)
 
 import_grids_btn = tk.Button(left_panel, text="Import Grids", command=import_grids, width=button_width)
 import_grids_btn.grid(row=1, column=0, padx=button_padx, pady=button_pady)
+edit_geocode_group_btn = tk.Button(left_panel, text="Edit Geocode Group", command=edit_geocode_group, width=button_width)
+edit_geocode_group_btn.grid(row=1, column=1, padx=button_padx, pady=button_pady)
 
 view_statistics_btn = tk.Button(left_panel, text="Set up processing", command=edit_susceptibilitiesandimportance, width=button_width)
 view_statistics_btn.grid(row=2, column=0, padx=button_padx, pady=button_pady)
