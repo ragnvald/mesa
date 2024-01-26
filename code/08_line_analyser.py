0509a2d72bac938ec9159fa13097aa7a45ca8da4
ragnvald <ragnvald@mindland.com>
2024-01-26 20:14:43 +0100

    PLanning new functionality.

---
+++ code/08_line_analyser.py
@@ -0,0 +1,15 @@
+#
+#
+#  User selects input geocode group
+#  List of lines is provided (tbl_line_in)
+#   1) No tbl_line_input means a line should be created based on tbl_flat.
+#   2) tbl_line_input has one of more lines
+#  User selects lines
+#  User selects:
+#     - buffered width of line in meters
+#     - line segment length
+#  Analyse overlayng geocode with line segments / polygons
+#     - results in tbl_line_out_flat (we might build a stacked one at a later stage...)
+#
+#
+#
