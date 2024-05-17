cd75d50de912319ac4e592005b95f0afb2d60e25
ragnvald <ragnvald@mindland.com>
2024-05-17 16:18:53 +0200

    File move: Compilation updated. All good now,  I hope.

--- code/compile_win.bat
+++ code/compile_win.bat
@@ -1,8 +1,9 @@
 :: Compilation script.
 :: Consider using UPX-co-compilation package for smaller files.
 
+@echo off
+
 echo Compiling Python scripts...
-echo off
 
 setlocal
 
@@ -18,27 +19,41 @@ set "SCRIPT_PATH=%SCRIPT_PATH:~0,-1%"
 :: Get the path of the parent directory
 for %%i in ("%SCRIPT_PATH%") do set "PARENT_DIR=%%~dpi"
 
+:: Echo the values for verification
+echo SCRIPT_PATH is %SCRIPT_PATH%
+echo PARENT_DIR is %PARENT_DIR%
+
 :: Path of the work folder relative to the script
 set "BUILD_FOLDER_ROOT=%PARENT_DIR%build"
 set "BUILD_FOLDER_SYSTEM=%PARENT_DIR%build\system"
-
 
 :: Define the dist folder path
 set "DIST_FOLDER_ROOT=%PARENT_DIR%dist"
 set "DIST_FOLDER_SYSTEM=%PARENT_DIR%dist\system"
 
-:: Define the system folder
-set "SOURCE_FOLDER_ROOT=%PARENT_DIR%"
-set "SOURCE_FOLDER_SYSTEM=%PARENT_DIR%system"
+:: Define the system folder path
+set "SCRIPT_FOLDER_ROOT=%SCRIPT_PATH%"
+set "SCRIPT_FOLDER_SYSTEM=%SCRIPT_PATH%\system"
 
-:: Ensure the target directory exists
+:: Echo the values for verification
+echo BUILD_FOLDER_ROOT is %BUILD_FOLDER_ROOT%
+echo BUILD_FOLDER_SYSTEM is %BUILD_FOLDER_SYSTEM%
+echo DIST_FOLDER_ROOT is %DIST_FOLDER_ROOT%
+echo DIST_FOLDER_SYSTEM is %DIST_FOLDER_SYSTEM%
+echo SCRIPT_FOLDER_ROOT is %SCRIPT_FOLDER_ROOT%
+echo SCRIPT_FOLDER_SYSTEM is %SCRIPT_FOLDER_SYSTEM%
+
+:: Ensure the target directories exist
+if not exist "%DIST_FOLDER_ROOT%" (
+    mkdir "%DIST_FOLDER_ROOT%"
+)
 if not exist "%DIST_FOLDER_SYSTEM%" (
     mkdir "%DIST_FOLDER_SYSTEM%"
 )
 
 echo
 echo Fetching config.ini...
-xcopy "%SCRIPT_PATH%\system\config.ini" "%DIST_FOLDER_SYSTEM%" /Y
+xcopy "%SCRIPT_FOLDER_SYSTEM%\config.ini" "%DIST_FOLDER_SYSTEM%" /Y
 if errorlevel 1 echo Error copying config.ini
 
 echo
@@ -69,52 +84,103 @@ echo Working in this folder: %BUILD_FOLDER_ROOT%
 echo Distribution folder will be: %DIST_FOLDER_ROOT%
 
 :: Start the compilation
-set "PYINSTALLER_CMD_ROOT=pyinstaller --onefile --collect-all ttkbootstrap --collect-all tkinterweb --hidden-import=ttkbootstrap --distpath=%DIST_FOLDER_ROOT% --workpath=%SOURCE_FOLDER_ROOT%"
-set "PYINSTALLER_CMD_SYSTEM=pyinstaller --onefile --collect-all ttkbootstrap --collect-all tkinterweb --hidden-import=ttkbootstrap --distpath=%DIST_FOLDER_ROOT% --workpath=%SOURCE_FOLDER_SYSTEM%"
+set "PYINSTALLER_CMD_ROOT=pyinstaller --onefile --collect-all ttkbootstrap --collect-all tkinterweb --hidden-import=ttkbootstrap --distpath=%DIST_FOLDER_ROOT% --workpath=%BUILD_FOLDER_ROOT% --add-data %SCRIPT_FOLDER_SYSTEM%;system --add-data %SCRIPT_PATH%\system_resources;system_resources"
+set "PYINSTALLER_CMD_SYSTEM=pyinstaller --onefile --collect-all ttkbootstrap --collect-all tkinterweb --hidden-import=ttkbootstrap --distpath=%DIST_FOLDER_SYSTEM% --workpath=%BUILD_FOLDER_SYSTEM% --add-data %SCRIPT_FOLDER_SYSTEM%;system --add-data %SCRIPT_PATH%\system_resources;system_resources"
 
-echo Working on mesa.py
-%PYINSTALLER_CMD_ROOT% mesa.py >nul 2>&1
+echo Working on mesa.py in %SCRIPT_FOLDER_ROOT%
+echo %PYINSTALLER_CMD_ROOT% "%SCRIPT_FOLDER_ROOT%\mesa.py"
+%PYINSTALLER_CMD_ROOT% "%SCRIPT_FOLDER_ROOT%\mesa.py" >nul 2>&1
 if errorlevel 1 echo Error compiling mesa.py
 
-echo Working on 01_import.py in %SOURCE_FOLDER_SYSTEM%
-%PYINSTALLER_CMD_SYSTEM% 01_import.py
-if errorlevel 1 echo Error compiling 01_import.py
+echo Working on 01_import.py in %SCRIPT_FOLDER_SYSTEM%
+if exist "%SCRIPT_FOLDER_SYSTEM%\01_import.py" (
+    echo %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\01_import.py"
+    %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\01_import.py" >nul 2>&1
+    if errorlevel 1 echo Error compiling 01_import.py
+) else (
+    echo File 01_import.py does not exist in %SCRIPT_FOLDER_SYSTEM%
+)
 
 echo Working on 02_present_files.py
-%PYINSTALLER_CMD_SYSTEM% 02_present_files.py >nul 2>&1
-if errorlevel 1 echo Error compiling 02_present_files.py
+if exist "%SCRIPT_FOLDER_SYSTEM%\02_present_files.py" (
+    echo %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\02_present_files.py"
+    %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\02_present_files.py" >nul 2>&1
+    if errorlevel 1 echo Error compiling 02_present_files.py
+) else (
+    echo File 02_present_files.py does not exist in %SCRIPT_FOLDER_SYSTEM%
+)
 
 echo Working on 04_edit_asset_group.py
-%PYINSTALLER_CMD_SYSTEM% 04_edit_asset_group.py >nul 2>&1
-if errorlevel 1 echo Error compiling 04_edit_asset_group.py
+if exist "%SCRIPT_FOLDER_SYSTEM%\04_edit_asset_group.py" (
+    echo %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\04_edit_asset_group.py"
+    %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\04_edit_asset_group.py" >nul 2>&1
+    if errorlevel 1 echo Error compiling 04_edit_asset_group.py
+) else (
+    echo File 04_edit_asset_group.py does not exist in %SCRIPT_FOLDER_SYSTEM%
+)
 
 echo Working on 04_edit_geocode_group.py
-%PYINSTALLER_CMD_SYSTEM% 04_edit_geocode_group.py >nul 2>&1
-if errorlevel 1 echo Error compiling 04_edit_geocode_group.py
+if exist "%SCRIPT_FOLDER_SYSTEM%\04_edit_geocode_group.py" (
+    echo %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\04_edit_geocode_group.py"
+    %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\04_edit_geocode_group.py" >nul 2>&1
+    if errorlevel 1 echo Error compiling 04_edit_geocode_group.py
+) else (
+    echo File 04_edit_geocode_group.py does not exist in %SCRIPT_FOLDER_SYSTEM%
+)
 
 echo Working on 04_edit_input.py
-%PYINSTALLER_CMD_SYSTEM% 04_edit_input.py >nul 2>&1
-if errorlevel 1 echo Error compiling 04_edit_input.py
+if exist "%SCRIPT_FOLDER_SYSTEM%\04_edit_input.py" (
+    echo %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\04_edit_input.py"
+    %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\04_edit_input.py" >nul 2>&1
+    if errorlevel 1 echo Error compiling 04_edit_input.py
+) else (
+    echo File 04_edit_input.py does not exist in %SCRIPT_FOLDER_SYSTEM%
+)
 
 echo Working on 06_process.py
-%PYINSTALLER_CMD_SYSTEM% 06_process.py >nul 2>&1
-if errorlevel 1 echo Error compiling 06_process.py
+if exist "%SCRIPT_FOLDER_SYSTEM%\06_process.py" (
+    echo %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\06_process.py"
+    %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\06_process.py" >nul 2>&1
+    if errorlevel 1 echo Error compiling 06_process.py
+) else (
+    echo File 06_process.py does not exist in %SCRIPT_FOLDER_SYSTEM%
+)
 
 echo Working on 07_edit_atlas.py
-%PYINSTALLER_CMD_SYSTEM% 07_edit_atlas.py >nul 2>&1
-if errorlevel 1 echo Error compiling 07_edit_atlas.py
+if exist "%SCRIPT_FOLDER_SYSTEM%\07_edit_atlas.py" (
+    echo %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\07_edit_atlas.py"
+    %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\07_edit_atlas.py" >nul 2>&1
+    if errorlevel 1 echo Error compiling 07_edit_atlas.py
+) else (
+    echo File 07_edit_atlas.py does not exist in %SCRIPT_FOLDER_SYSTEM%
+)
 
 echo Working on 07_make_atlas.py
-%PYINSTALLER_CMD_SYSTEM% 07_make_atlas.py >nul 2>&1
-if errorlevel 1 echo Error compiling 07_make_atlas.py
+if exist "%SCRIPT_FOLDER_SYSTEM%\07_make_atlas.py" (
+    echo %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\07_make_atlas.py"
+    %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\07_make_atlas.py" >nul 2>&1
+    if errorlevel 1 echo Error compiling 07_make_atlas.py
+) else (
+    echo File 07_make_atlas.py does not exist in %SCRIPT_FOLDER_SYSTEM%
+)
 
 echo Working on 08_admin_lines.py
-%PYINSTALLER_CMD_SYSTEM% 08_admin_lines.py >nul 2>&1
-if errorlevel 1 echo Error compiling 08_admin_lines.py
+if exist "%SCRIPT_FOLDER_SYSTEM%\08_admin_lines.py" (
+    echo %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\08_admin_lines.py"
+    %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\08_admin_lines.py" >nul 2>&1
+    if errorlevel 1 echo Error compiling 08_admin_lines.py
+) else (
+    echo File 08_admin_lines.py does not exist in %SCRIPT_FOLDER_SYSTEM%
+)
 
 echo Working on 08_edit_lines.py
-%PYINSTALLER_CMD_SYSTEM% 08_edit_lines.py >nul 2>&1
-if errorlevel 1 echo Error compiling 08_edit_lines.py
+if exist "%SCRIPT_FOLDER_SYSTEM%\08_edit_lines.py" (
+    echo %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\08_edit_lines.py"
+    %PYINSTALLER_CMD_SYSTEM% "%SCRIPT_FOLDER_SYSTEM%\08_edit_lines.py" >nul 2>&1
+    if errorlevel 1 echo Error compiling 08_edit_lines.py
+) else (
+    echo File 08_edit_lines.py does not exist in %SCRIPT_FOLDER_SYSTEM%
+)
 
 echo Compilation complete. You will find the compiled code here: %DIST_FOLDER_ROOT%
 
@@ -139,3 +205,6 @@ echo
 del "*.spec" /q
 
 echo All .spec-files deleted
+echo
+echo Compilation completed
+
